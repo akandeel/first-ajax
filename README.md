70bc7a703d5bba07fff1d1fad21acd742e723192
your github.com username <kandeel.a1@gmail.com>
2017-02-17 01:33:58 -0500

    complete step 0 - step 8. step 9 needs a .done and a .append.

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/cohorts/67/assignments/2055/latest)
+
+
+STEP 0
+
+1.
+the browser used a GET request method.
+
+2.
+122 ms
+
+3.
+200. this means the request was fullfilled.
+
+4.
+initiator is the object or process that nitiiated the request. It can have one of the following:
+* parser
+* redirect
+* script
+
+-> in this case it is other, meaning it was a request made by a user. not from redirect or script.
+
+
+STEP 1
+
+1.
--- ajax.js
+++ ajax.js
@@ -1,5 +1,60 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-  /* Your code goes here */
+  $('#request-root').on('click', function() {
 
+      // AJAX REQUEST
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/',
+      method: 'GET',
+      // data: ,
+      dataType: 'text'
+    });
+  });
+
+      // CHAINED FUNCTION .DONE AT END OF AJAX. PASS ANOTHER FUNCTION AT THE END OF .DONE
+  $('#request-ping').on('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/ping',
+      method: 'GET',
+      dataType: 'text'
+    }).done(function(responseData) {
+      console.log(responseData);
+      console.log('The request is done.');
+      $('#step3,4,5,6').append(responseData);
+    }).fail(function() {
+      console.log('The request failed.');
+      $('#step3,4,5,6').append('Try harder next time!')
+
+      // .ALWAYS FUNCTION PREVENTS DUPLICATION IN .DONE AND .FAIL CALLBACKS
+    }).always(function() {
+      console.log('This will always pass.');
+      $('body').append('Hey the request finished!')
+    });
+  });
+  $('#hive-mind').on('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      method: 'GET',
+      dataType: 'text'
+    }).done(function(responseData) {
+      console.log(responseData);
+      $('#step7').append(responseData);
+    });
+  });
+  $('#send-data').on('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method: 'GET',
+      dataType: 'text'
+    }).done(function(responseData) {
+      $('#step8').append(responseData);
+    });
+  });
+  $('#request-car').on('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/a_car',
+      method: 'GET',
+      dataType: 'HTML'
+  // STILL NEED .DONE AND APPEND IT.
+  });
 });
